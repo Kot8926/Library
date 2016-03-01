3312a87e27fd1ff0cbe691692816b9305ce7cb71
Sergey <kotjara25@mail.ru>
2016-03-01 11:51:08 +0300

    +BookController +View List +Правил роут

---
+++ Library.WebUI/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Domain.Abstract;
+using Library.Domain.Entities;
+
+namespace Library.WebUI.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository reposit;
+        public BookController(IBookRepository bookRep)
+        {
+            this.reposit = bookRep;   
+        }
+
+        public ViewResult List()
+        {
+            return View(reposit.Books);
+        }
+
+    }
+}
