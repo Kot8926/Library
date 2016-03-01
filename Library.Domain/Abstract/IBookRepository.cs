051eb756697d373da39208f9acce6bba3b9c20bd
Sergey <kotjara25@mail.ru>
2016-03-01 11:12:28 +0300

    Добавил Book и IBookRepository

---
+++ Library.Domain/Abstract/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using Library.Domain.Entities;
+using System.Linq;
+
+
+namespace Library.Domain.Abstract
+{
+    public interface IBookRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
