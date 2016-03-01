09c583feca0ebf0043e1f3ceda2577e2ae7fca1a
Sergey <kotjara25@mail.ru>
2016-03-01 10:47:02 +0300

    Добавил фабрику контроллеров. 1-й запуск.

---
+++ Library.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+
+namespace Library.WebUI.Infrastructure
+{
+    //Создаем пользовательскую фабрику обьектов.
+    //Наследуясь от стандартной.
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private StandardKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            //Создаем контейнер
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            //Получение контроллера из контейнера используя
+            //его тип
+            return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        //Установить зависимости
+        private void AddBindings()
+        {
+
+        }
+    }
+}
