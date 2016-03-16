63750b50ddfa75e4e47678215449ecd4db8fd0de
Sergey <kotjara25@mail.ru>
2016-03-16 13:29:51 +0300

    +Cart класс реализующий корзину и тесты для для него

---
+++ Library.Domain/Entities/Cart.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Domain.Entities;
+
+namespace Library.Domain.Entities
+{
+    //Информация о одной книге в корзине
+    public class CartLine
+    {
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+
+
+    public class Cart
+    {
+        //Контейнер для книг в корзине
+        private List<CartLine> bookList  = new List<CartLine>();
+
+        //Добавление книги
+        public void AddBook(Book book, int quantity)
+        {
+            //Проверяем есть ли такой элемент в контейнере
+            CartLine item = bookList.Where(b => b.Book.BookId == book.BookId).FirstOrDefault();
+
+            if (item == null)
+            {
+                bookList.Add(new CartLine { Book = book, Quantity = quantity });
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        //Удаление книги
+        public void DeleteBook(Book book)
+        {
+            bookList.RemoveAll(b => b.Book.BookId == book.BookId);
+        }
+
+        //Очистить контейнер
+        public void ClearAll()
+        {
+            bookList.Clear();
+        }
+
+        //Стоимость возмещения при утрате
+        public decimal CostForLost()
+        {
+            return bookList.Sum(s => s.Book.PriceLoss * s.Quantity);
+        }
+
+        //Доступ к содержимому
+        public IEnumerable<CartLine> Lines
+        {
+            get { return bookList;  }
+        }
+    }
+}
---
+++ Library.UnitTest/CartTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Library.Domain.Entities;
+
+namespace Library.UnitTest
+{
+    [TestClass]
+    public class CartTest
+    {
+        //Добавление нового элемента в корзину------------------------------------------------------------------------------
+        [TestMethod]
+        public void Add_New_Book_Test()
+        {
+            //Arrage
+            Book book1 = new Book { BookId = 1, Name = "book1" };
+            Book book2 = new Book { BookId = 2, Name = "book2" };
+
+            Cart target = new Cart();
+
+            //Act
+            target.AddBook(book1, 1);
+            target.AddBook(book2, 1);
+            CartLine[] result = target.Lines.ToArray();
+
+
+            //Assert
+            Assert.AreEqual(result[0].Book.Name, book1.Name);
+            Assert.AreEqual(result[1].Book.Name, book2.Name);
+            Assert.IsTrue(result.Length == 2);
+        }
+
+        //Добавление существующего элемента в корзину----------------------------------------------------------------------
+        [TestMethod]
+        public void Add_Quantity_For_Existing_Book()
+        {
+            //Arrage
+            Book book1 = new Book { BookId = 1, Name = "book1" };
+            Book book2 = new Book { BookId = 2, Name = "book2" };
+
+            Cart target = new Cart();
+
+            //Act
+            target.AddBook(book1, 1);
+            target.AddBook(book2, 1);
+            target.AddBook(book1, 10);
+
+            //Assert
+            CartLine[] result = target.Lines.ToArray();
+            Assert.IsTrue(result.Length == 2);
+            Assert.AreEqual(result[0].Quantity, 11);
+        }
+
+        //Удаление элемента из корзины--------------------------------------------------------------------------------------
+        [TestMethod]
+        public void Delete_Book()
+        {
+            //Arrage
+            Book book1 = new Book { BookId = 1, Name = "book1" };
+            Book book2 = new Book { BookId = 2, Name = "book2" };
+            Book book3 = new Book { BookId = 3, Name = "book3" };
+
+            Cart target = new Cart();
+
+            target.AddBook(book1, 1);
+            target.AddBook(book2, 1);
+            target.AddBook(book3, 10);
+            target.AddBook(book2, 3);
+
+            //Act
+            target.DeleteBook(book2);
+
+            //Assert
+            Assert.AreEqual(target.Lines.Where(b => b.Book.BookId == 2).Count(), 0);
+            Assert.IsTrue(target.Lines.Count() == 2);
+        }
+
+        //Рассчет общей суммы при утрате-------------------------------------------------------------------------------------
+        [TestMethod]
+        public void Calculate_Price_Lost()
+        {
+            //Arrage
+            Book book1 = new Book { BookId = 1, PriceLoss = 10 };
+            Book book2 = new Book { BookId = 2, PriceLoss = 2 };
+            Book book3 = new Book { BookId = 3,  PriceLoss = 5 };
+
+            Cart target = new Cart();
+
+            //Act
+            target.AddBook(book1, 3);
+            target.AddBook(book2, 2);
+            target.AddBook(book3, 1);
+            decimal result = target.CostForLost();
+
+            //Assert
+            Assert.AreEqual(result, 39M);
+        }
+
+        //Очистить контейнер--------------------------------------------------------------------------------------------------
+        [TestMethod]
+        public void Clear_All_Cart()
+        {
+            //Arrage
+            Book book1 = new Book { BookId = 1, Name = "book1" };
+            Book book2 = new Book { BookId = 2, Name = "book2" };
+            Book book3 = new Book { BookId = 3, Name = "book3" };
+
+            Cart target = new Cart();
+
+            target.AddBook(book1, 1);
+            target.AddBook(book2, 1);
+            target.AddBook(book3, 10);
+            target.AddBook(book2, 3);
+
+            //Act
+            target.ClearAll();
+
+            //Assert
+            Assert.IsTrue(target.Lines.Count() == 0);
+        }
+    }
+}
