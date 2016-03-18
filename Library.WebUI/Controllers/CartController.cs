17bcd71e15157217f39322af4767bb0103b4b0d8
Sergey <kotjara25@mail.ru>
2016-03-18 10:56:42 +0300

    +Кнопку добавления товара в корзину. Начальный функционал корзины.

---
+++ Library.WebUI/Controllers/CartController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Domain.Abstract;
+using Library.Domain.Entities;
+using Library.WebUI.Models;
+
+namespace Library.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IBookRepository reposit;
+        public CartController(IBookRepository bookRep)
+        {
+            reposit = bookRep;
+        }
+
+        //Создаем сессию. Для сохранения и извлечения обьектов
+        private Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+
+        //Помещаем обьект в корзину. Перенаправляем к представлению Index.
+        public RedirectToRouteResult AddToCart(int bookId, string returnUrl)
+        {
+            Book book = reposit.Books.FirstOrDefault(b => b.BookId == bookId);
+            if (book != null)
+            {
+                GetCart().AddBook(book, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        //Удаляем обьект из корзины. Перенаправляем к Index
+        public RedirectToRouteResult DeleteToCart(int bookId, string returnUrl)
+        {
+            Book book = reposit.Books.FirstOrDefault(b => b.BookId == bookId);
+            if (book != null)
+            {
+                GetCart().DeleteBook(book);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel 
+            { 
+                Cart = GetCart(),
+                ReturnUrl = returnUrl,
+            });
+        }
+    }
+}
---
+++ Library.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.WebUI.Models
+{
+    //Модель представления для передачи в Index
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
