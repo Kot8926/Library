cc0c1f2a8736f63f677bed20d5eb7524bc218038
Sergey <kotjara25@mail.ru>
2016-03-22 12:24:28 +0300

    +Админская часть +Unit тест контроллеру Admin +View Index

---
+++ Library.UnitTest/AdminTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library.Domain.Abstract;
+using Library.Domain.Entities;
+using Library.WebUI.Controllers;
+
+
+namespace Library.UnitTest
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void Index_Contains_All_Books()
+        {
+            //Arrange
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book> {
+            new Book {BookId = 1, Name = "book1"},
+            new Book {BookId = 2, Name = "book2"},
+            new Book {BookId = 3, Name = "book3"},
+            }.AsQueryable());
+
+            AdminController target = new AdminController(mock.Object);
+
+            //Act
+            Book[] result = ((IEnumerable<Book>)target.Index().Model).ToArray();
+
+            //Assert
+            Assert.AreEqual(result[0].Name, "book1");
+            Assert.AreEqual(result.Length, 3);
+        }
+    }
+}
---
+++ Library.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Library.Domain.Abstract;
+
+namespace Library.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IBookRepository repository;
+        public AdminController(IBookRepository reposit)
+        {
+            repository = reposit;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Books);
+        }
+
+    }
+}
